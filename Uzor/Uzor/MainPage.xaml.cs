eba1c596b58b9f4c244a5bbd66f74ff303340bf1
Ljachovsky <40249013+Ljachovsky@users.noreply.github.com>
2021-03-26 01:55:30 +0300

    new project

---
+++ Uzor/Uzor/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uzor.Views;
+using Xamarin.Forms;
+
+namespace Uzor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void add_item(object sender, EventArgs e)
+        {
+            list.Children.Add(new UzorItem());
+        }
+    }
+}
